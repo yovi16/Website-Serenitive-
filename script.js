7aa79ed3e6d76e2564e6ffd4cfb78ff7257f1626
yovi16 <yovidelindra125@gmail.com>
2024-12-08 18:34:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Pesan Anda telah dikirim!');
+});
+
+document.getElementById('signup-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Pendaftaran berhasil!');
+});
